236445ad29fd1491c1e35d77c41d9182886f918e
tigawanna <denniskinuthiaw@gmail.com>
2026-04-18 15:04:25 +0300

    Add initial Repositories component with basic layout

---
+++ apps/web/src/routes/_public/-components/Repositories.tsx
@@ -0,0 +1,7 @@
+interface RepositoriesProps {}
+
+export function Repositories({}: RepositoriesProps) {
+  return (
+    <div className="w-full h-full flex flex-col items-center justify-center">repositories</div>
+  );
+}
