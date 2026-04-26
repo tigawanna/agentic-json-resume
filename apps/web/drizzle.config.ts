97a167efb8532134403847d484f2c9dc89b0d9fe
tigawanna <denniskinuthiaw@gmail.com>
2026-04-26 20:05:25 +0300

    chore: update database configuration to use Turso dialect
    - Changed the database dialect from SQLite to Turso for improved compatibility.
    - Added support for an authentication token in the database credentials, enhancing security and flexibility in environment variable management.

--- apps/web/drizzle.config.ts
+++ apps/web/drizzle.config.ts
@@ -3,8 +3,9 @@ import { defineConfig } from "drizzle-kit";
 export default defineConfig({
   schema: "./src/lib/drizzle/scheam/**/*.ts",
   out: "./drizzle/migrations",
-  dialect: "sqlite",
+  dialect: "turso",
   dbCredentials: {
     url: process.env.DATABASE_URL ?? "file:./db.sqlite",
+    authToken: process.env.DATABASE_AUTH_TOKEN ?? "",
   },
 });
